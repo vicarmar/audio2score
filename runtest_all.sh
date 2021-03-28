9c272f1169236b2317ed1f2ce863474ea51d3594
Victor Arroyo <vicarmar@hotmail.com>
2021-03-28 21:44:07 +0200

    Enable 32 tests for multi, multi2 w/wo splits

--- runtest_all.sh
+++ runtest_all.sh
@@ -1,14 +1,12 @@
-# ./runtest.sh ../datasets/prep/quartets_v0/quartets_multi/test_quartets.csv ./models/quartets_multi.pth
-# ./runtest.sh ../datasets/prep/quartets_v0/quartets_multi/test_quartets.csv ./models/quartets_multi_splits.pth
-# ./runtest.sh ../datasets/prep/quartets_v0/quartets_multi/test_quartets.csv ./models/quartets_multi2.pth
-# ./runtest.sh ../datasets/prep/quartets_v0/quartets_multi/test_quartets.csv ./models/quartets_multi2_splits.pth
+./runtest.sh ../datasets/prep/quartets_v0/quartets_multi/test_quartets.csv ./models/quartets_multi.pth
+./runtest.sh ../datasets/prep/quartets_v0/quartets_multi/test_quartets.csv ./models/quartets_multi_splits.pth
+./runtest.sh ../datasets/prep/quartets_v0/quartets_multi/test_quartets.csv ./models/quartets_multi2.pth
+./runtest.sh ../datasets/prep/quartets_v0/quartets_multi/test_quartets.csv ./models/quartets_multi2_splits.pth
 
-# ./runtest.sh ../datasets/prep/quartets_v0/quartets_multi/val_quartets.csv ./models/quartets_multi.pth
-# ./runtest.sh ../datasets/prep/quartets_v0/quartets_multi/val_quartets.csv ./models/quartets_multi_splits.pth
-# ./runtest.sh ../datasets/prep/quartets_v0/quartets_multi/val_quartets.csv ./models/quartets_multi2.pth
-# ./runtest.sh ../datasets/prep/quartets_v0/quartets_multi/val_quartets.csv ./models/quartets_multi2_splits.pth
-# DONE
-
+./runtest.sh ../datasets/prep/quartets_v0/quartets_multi/val_quartets.csv ./models/quartets_multi.pth
+./runtest.sh ../datasets/prep/quartets_v0/quartets_multi/val_quartets.csv ./models/quartets_multi_splits.pth
+./runtest.sh ../datasets/prep/quartets_v0/quartets_multi/val_quartets.csv ./models/quartets_multi2.pth
+./runtest.sh ../datasets/prep/quartets_v0/quartets_multi/val_quartets.csv ./models/quartets_multi2_splits.pth
 
 ./runtest.sh ../datasets/prep/quartets_v0/quartets_multi2/test_quartets.csv ./models/quartets_multi.pth
 ./runtest.sh ../datasets/prep/quartets_v0/quartets_multi2/test_quartets.csv ./models/quartets_multi_splits.pth
